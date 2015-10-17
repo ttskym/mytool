8dda4ca812dbbe9d4677afabd4764b9687668a8c
刘惊坤 <liujkon@gmail.com>
2015-10-17 22:43:01 +0800

    1

--- vps_config/vps.sh
+++ vps_config/vps.sh
@@ -33,7 +33,7 @@ if [[ ! -z $popt ]]
 then
 ## PPTP configuration
 
-rpm -Uvh epel-release-7*.rpm
+rpm -i http://poptop.sourceforge.net/yum/stable/rhel6/pptp-release-current.noarch.rpm
 yum -y install pptpd || apt-get -y install pptpd
 ppp_pptp=/etc/ppp/pptpd-options   #不同发行版的该文件名称不同
 if [[ ! -z $ppp_pptp ]]
